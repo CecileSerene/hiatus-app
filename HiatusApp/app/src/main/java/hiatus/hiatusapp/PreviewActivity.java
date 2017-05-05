5648eaf01d1706c457cf6daed51230ba60c783b2
Cécile <cecile.gontier@student.ecp.fr>
2017-05-05 16:54:06 +0200

    having a preview of the text

--- HiatusApp/app/src/main/java/hiatus/hiatusapp/PreviewActivity.java
+++ HiatusApp/app/src/main/java/hiatus/hiatusapp/PreviewActivity.java
@@ -1,13 +1,25 @@
 package hiatus.hiatusapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class PreviewActivity extends AppCompatActivity {
+
+    TextView preview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_preview);
+
+        preview = (TextView) findViewById(R.id.text_preview);
+
+        Intent intent = getIntent();
+        String contribution = intent.getExtras().getString("contribution");
+        preview.setText(contribution);
+        
+
     }
 }
