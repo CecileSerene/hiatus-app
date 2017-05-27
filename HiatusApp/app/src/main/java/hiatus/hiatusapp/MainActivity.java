30b0d2fdc7616b16dca522365c373177cff0ba5e
Manca Florimond <florimond.manca@student.ecp.fr>
2017-05-27 21:55:09 +0200

    Main activity -> contribution activity

--- HiatusApp/app/src/main/java/hiatus/hiatusapp/MainActivity.java
+++ HiatusApp/app/src/main/java/hiatus/hiatusapp/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Intent intent = new Intent(this, LoginActivity.class);
+        Intent intent = new Intent(this, ContributionActivity.class);
         startActivity(intent);
     }
 }
