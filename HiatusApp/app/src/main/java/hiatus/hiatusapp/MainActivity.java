fc81dd887f85517be6398f9d71dc60711488c492
Manca Florimond <florimond.manca@student.ecp.fr>
2017-05-28 23:20:52 +0200

    Start with LoginActivity again

--- HiatusApp/app/src/main/java/hiatus/hiatusapp/MainActivity.java
+++ HiatusApp/app/src/main/java/hiatus/hiatusapp/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Intent intent = new Intent(this, MenuActivity.class);
+        Intent intent = new Intent(this, LoginActivity.class);
         startActivity(intent);
     }
 }
