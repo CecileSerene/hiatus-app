a2449b0e2a61d7f8a43f637c5be29c8d94c327a2
Manca Florimond <florimond.manca@student.ecp.fr>
2017-05-29 21:21:36 +0200

    quickfix

--- HiatusApp/app/src/main/java/hiatus/hiatusapp/MainActivity.java
+++ HiatusApp/app/src/main/java/hiatus/hiatusapp/MainActivity.java
@@ -3,6 +3,7 @@ package hiatus.hiatusapp;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 
 import hiatus.hiatusapp.Menu.MenuActivity;
 
