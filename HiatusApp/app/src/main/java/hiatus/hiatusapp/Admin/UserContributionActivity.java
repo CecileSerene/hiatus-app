38797988c76cccaf5eaf4ad9be872778a8a109df
Cécile <cecile.gontier@student.ecp.fr>
2017-05-31 23:15:42 +0200

    usercontributionlistview

--- HiatusApp/app/src/main/java/hiatus/hiatusapp/Admin/UserContributionActivity.java
+++ HiatusApp/app/src/main/java/hiatus/hiatusapp/Admin/UserContributionActivity.java
@@ -3,14 +3,35 @@ package hiatus.hiatusapp.Admin;
 import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
 
+import java.util.ArrayList;
+
+import hiatus.hiatusapp.ContributionBundle.ContributionBundle;
+import hiatus.hiatusapp.ContributionContent.TextContent;
 import hiatus.hiatusapp.R;
 
 public class UserContributionActivity extends Activity {
+
+    private ArrayList<ContributionBundle> bundles;
+    ListView lvBundle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_contribution);
+        
+        //exemple
+        bundles = new ArrayList<ContributionBundle>();
+        bundles.add(new ContributionBundle("0", "1", new TextContent("1")));
+        bundles.add(new ContributionBundle("1", "2", new TextContent("2")));
+        //
+
+        lvBundle = (ListView) findViewById(R.id.bundle_list);
+
+        lvBundle.setAdapter(new ContributionBundleArrayAdapter(this, bundles));
+
+        //TODO set an onclickitemlistener which redirects to different activities textpreview or photopreview
+
     }
 }
--- HiatusApp/app/src/main/java/hiatus/hiatusapp/LoginActivity.java
+++ HiatusApp/app/src/main/java/hiatus/hiatusapp/LoginActivity.java
@@ -28,6 +28,7 @@ import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.UserProfileChangeRequest;
 
 import hiatus.hiatusapp.Admin.AdminActivity;
+import hiatus.hiatusapp.Admin.UserContributionActivity;
 import hiatus.hiatusapp.Menu.MenuActivity;
 
 
@@ -65,7 +66,7 @@ public class LoginActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        startActivity(new Intent(LoginActivity.this, AdminActivity.class));
+        startActivity(new Intent(LoginActivity.this, UserContributionActivity.class));
 
         // Setup firebase authentication
         mAuth = FirebaseAuth.getInstance();
