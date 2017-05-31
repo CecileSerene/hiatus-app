61249f884134d677e9f7abc3b5df9348a5358bc8
Cécile <cecile.gontier@student.ecp.fr>
2017-05-31 22:17:19 +0200

    admin part layout

---
+++ HiatusApp/app/src/main/java/hiatus/hiatusapp/Admin/ContributionPreviewActivity.java
@@ -0,0 +1,15 @@
+package hiatus.hiatusapp.Admin;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import hiatus.hiatusapp.R;
+
+public class ContributionPreviewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contribution_preview);
+    }
+}
