ed4c99c13a6869b588e05cb93c6ceba7292d27a4
Manca Florimond <florimond.manca@student.ecp.fr>
2017-06-06 18:05:04 +0200

    Make History fragment show contributions of the user

--- HiatusApp/app/src/main/java/hiatus/hiatusapp/menu/MenuHistoryFragment.java
+++ HiatusApp/app/src/main/java/hiatus/hiatusapp/menu/MenuHistoryFragment.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.ListFragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,8 +13,18 @@ import android.widget.TextView;
 
 import com.firebase.ui.database.FirebaseListAdapter;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
+
+import hiatus.hiatusapp.admin.ContributionBundleArrayAdapter;
+import hiatus.hiatusapp.admin.PhotoContributionPreviewActivity;
+import hiatus.hiatusapp.admin.TextContributionPreviewActivity;
+import hiatus.hiatusapp.contribution.base.ContributionBundle;
+import hiatus.hiatusapp.contribution.base.ContributionContent;
 import hiatus.hiatusapp.contribution.base.ContributionContext;
 import hiatus.hiatusapp.DatabaseHelper;
 import hiatus.hiatusapp.R;
@@ -23,7 +34,11 @@ import hiatus.hiatusapp.R;
  */
 public class MenuHistoryFragment extends ListFragment {
 
-    private FirebaseListAdapter<ContributionContext> mAdapter;
+    private static final String TAG = "MenuHistoryFragment";
+
+    private ContributionBundleArrayAdapter mAdapter;
+    private ArrayList<ContributionBundle> mBundles;
+    private ArrayList<ContributionContent> mContents;
 
     public static Fragment newInstance() {
         return new MenuHistoryFragment();
@@ -33,21 +48,35 @@ public class MenuHistoryFragment extends ListFragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_menu_history, container, false);
 
+        mBundles = new ArrayList<>();
+        mContents = new ArrayList<>();
+
+        mAdapter = new ContributionBundleArrayAdapter(getActivity(), mBundles);
+
         Query myContextsQuery = DatabaseHelper
                 // take bundles whose userId is current logged-in user's id
                 .getContributionBundleReference()
-                .equalTo(FirebaseAuth.getInstance().getCurrentUser().getUid(), "userUid");
+                .orderByKey()
+                .equalTo(FirebaseAuth.getInstance().getCurrentUser().getUid());
 
-        mAdapter = new FirebaseListAdapter<ContributionContext>(
-                getActivity(), ContributionContext.class, R.layout.fragment_menu_list_item, myContextsQuery) {
+        myContextsQuery.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
-            protected void populateView(View v, ContributionContext model, int position) {
-                TextView title = (TextView) v.findViewById(R.id.contribution_title);
-                TextView theme = (TextView) v.findViewById(R.id.contribution_theme);
-                title.setText(model.getTitle());
-                theme.setText(model.getTheme());
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                DataSnapshot userSnapshot = dataSnapshot.child(FirebaseAuth.getInstance().getCurrentUser().getUid());
+                for (DataSnapshot bundleSnapshot : userSnapshot.getChildren()) {
+                    ContributionBundle bundle = bundleSnapshot.getValue(ContributionBundle.class);
+                    mBundles.add(bundle);
+                    mContents.add(DatabaseHelper.retrieveContent(bundle));
+                    Log.d(TAG, "loadBundle:" + bundle.getId() + bundle.getContentModel().getTitle());
+                }
+                mAdapter.notifyDataSetChanged();
             }
-        };
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Log.w(TAG, "loadBundles:onCancelled", databaseError.toException());
+            }
+        });
         setListAdapter(mAdapter);
 
 
@@ -58,16 +87,17 @@ public class MenuHistoryFragment extends ListFragment {
     public void onListItemClick(ListView l, View v, int position, long id) {
         super.onListItemClick(l, v, position, id);
 
-        ContributionContext context = mAdapter.getItem(position);
+        ContributionBundle bundle = mBundles.get(position);
+        ContributionContent content = mContents.get(position);
 
-        // populate an intent with the contribution context
-        Intent i = new Intent(getActivity(), ContributionHistoryDetail.class);
-        i.putExtra("context",context);
-        startActivity(i);
-        // TODO replace these with the contribution-specific data
-        i.putExtra("date", "01/01/2001");
-        i.putExtra("state", "Accepté");
-
+        Intent i = new Intent(
+                getActivity(),
+                bundle.getContentModel().getType() == ContributionContent.Model.TYPE_TEXT ?
+                        TextContributionPreviewActivity.class : PhotoContributionPreviewActivity.class
+        );
+        i.putExtra("content", content);
+        i.putExtra("date", bundle.getDate());
+        i.putExtra("state", bundle.getState());
         startActivity(i);
     }
 }
